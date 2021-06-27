a5b7ad76a8106b14e832fedd2a7b746d23e5f790
NguyenThiNgocAnh <nguyenngocanh21012000@gmail.com>
2021-06-27 20:02:37 +0700

    sua mot chut

--- QuanLiKhoBanHang/Dataconn.cs
+++ QuanLiKhoBanHang/Dataconn.cs
@@ -20,7 +20,7 @@ namespace QuanLiKhoBanHang
         static Dataconn()
           
         {
-            source = @"Data Source=ADMIN;Initial Catalog=QLKBH_HK;Integrated Security=True";
+            source = @"Data Source=NGUYEN;Initial Catalog=QLKBH_HK;User ID=sa;Password=sa";
             con = new SqlConnection(source);
             if (con.State == ConnectionState.Open)
             {
